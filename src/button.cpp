127afa26d6afeeed339e69a39b4a0072144f904f
Ihor-imp <zencevihor@gmail.com>
2026-06-08 14:13:29 +0300

    Add begin and isPressed methods

---
+++ src/button.cpp
@@ -0,0 +1,17 @@
+#include "button.h"
+
+Button::Button(uint8_t pin)
+    : pin(pin)
+{
+
+}
+
+void Button::begin()
+{
+   pinMode(pin, INPUT_PULLUP);
+}
+
+bool Button::isPressed()
+{
+    return digitalRead(pin) == LOW;
+}
