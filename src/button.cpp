31a5780d3a1843e6becfae646aebceaacbadb0d4
Ihor-imp <zencevihor@gmail.com>
2026-06-08 15:08:30 +0300

    Add new debounce method

--- src/button.cpp
+++ src/button.cpp
@@ -3,15 +3,39 @@
 Button::Button(uint8_t pin)
     : pin(pin)
 {
-
 }
 
 void Button::begin()
 {
    pinMode(pin, INPUT_PULLUP);
+   lastReading = digitalRead(pin);
+   currentState = lastReading;
+   lastState = currentState;
+}
+
+
+void Button::debounce()
+{
+    reading = digitalRead(pin);
+    if (reading != lastReading)
+    {
+        lastDebounceTime = millis();
+        lastReading = reading;
+    }
+    if ((millis() - lastDebounceTime >= timeDebounce) && currentState != lastReading)
+    {
+        lastState = currentState;
+        currentState = lastReading;
+    }
+    
+}
+
+void Button::update()
+{    
+    debounce();
 }
 
 bool Button::isPressed()
 {
-    return digitalRead(pin) == LOW;
+    return currentState == LOW;
 }
--- src/button.h
+++ src/button.h
@@ -6,11 +6,22 @@ class Button
 {
 private:
     const uint8_t pin;
+    uint8_t timeDebounce = 50;
+    uint32_t lastDebounceTime = 0;
+
+    bool currentState = false;
+    bool lastState = false;
+
+    bool reading = false;
+    bool lastReading = false;
+
+    void debounce();
 
 public:
     Button(uint8_t pin);
 
     void begin();
+    void update();
     bool isPressed();
 
 };
